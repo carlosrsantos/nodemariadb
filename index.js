f834c367b2469a0d4fe2f785ea1b8262d3f27814
carlosrsantos <66956614+carlosrsantos@users.noreply.github.com>
2020-09-10 16:56:36 -0300

    Final do curso

--- index.js
+++ index.js
@@ -9,14 +9,22 @@ http.createServer(function(req, res) {
 //Usando Express
 const express = require('express');
 const app = express();
+
 const handlebars = require('express-handlebars');
+const Handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+
+
 const bodyParser = require('body-parser');
+
+//Variável de conexao com o banco de dados
 const Post = require('./models/Post');
 
 //Config 
     //Template Engine
 app.engine('handlebars', handlebars({
-    defaultLayout: 'main'}
+    defaultLayout: 'main',
+    handlebars: allowInsecurePrototypeAccess(Handlebars)}
  ));
 app.set('view engine', 'handlebars');
 
@@ -25,8 +33,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
+//Rotas
+app.get('/', (req, res) => {
+    Post.findAll({order: [['id', 'ASC']]}).then((params) => {
+       res.render('home', { params: params })
+    })
+});
 
-//Rotas 
+
 app.get('/cad', (req, res) => {    
     res.render('formulario');
 });
@@ -36,9 +50,17 @@ app.post('/add', (req, res) => {
         titulo: req.body.titulo,
         conteudo: req.body.conteudo
     }).then(()=>{
-        res.send('Post criado com sucesso!');
+        res.redirect('/');
     }).catch((error)=>{
         res.send(`Houve um erro: ${error}`);
+    })
+});
+
+app.get('/deletar/:id', (req, res)=> {
+    Post.destroy({where: {'id': req.params.id}}).then(()=>{
+        res.redirect('/');
+    }).catch((error)=>{
+        res.send(`Postagem inexistende ${error}`);
     })
 });
 
